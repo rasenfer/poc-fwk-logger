a2054c55ef238d1abae0a291c93cfdb8f636280d
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-06-25 07:52:50 +0200

    [ADD] repository annotation aspect

--- src/main/java/poc/fwk/logger/configurers/LoggerAopRepositoryConfigurer.java
+++ src/main/java/poc/fwk/logger/configurers/LoggerAopRepositoryConfigurer.java
@@ -6,34 +6,26 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.Repository;
 
 @Configuration
 @Aspect
-@ConditionalOnClass(Repository.class)
-public class LoggerAopJpaConfigurer extends LoggerAopBase {
+public class LoggerAopRepositoryConfigurer extends LoggerAopBase {
 
 	@Autowired
-	public LoggerAopJpaConfigurer(
+	public LoggerAopRepositoryConfigurer(
 			@Value("${poc.fwk.logger.repository.enabled:#{null}}") Boolean loggerRepositoryEnabled,
 			@Value("${poc.fwk.logger.repository.level:#{null}}") String loggerRepositoryLevel) {
 		super(loggerRepositoryEnabled, loggerRepositoryLevel);
 	}
 
 	@Pointcut("@within(org.springframework.stereotype.Repository)")
-	public void annotatedRepository() {
-		// Method is empty as this is just a Pointcut, the implementations are in the advices.
-	}
-
-	@Pointcut("within(org.springframework.data.repository.Repository+)")
 	public void repository() {
 		// Method is empty as this is just a Pointcut, the implementations are in the advices.
 	}
 
-	@Around("repository() && !annotatedLogger() && !annotatedRepository()")
-	public Object interceptRepository(ProceedingJoinPoint joinPoint) throws Throwable {
+	@Around("repository() && !annotatedLogger()")
+	public Object interceptService(ProceedingJoinPoint joinPoint) throws Throwable {
 		return interceptJoinPoint(joinPoint);
 	}
 
